bfa22b0ed3437876692cf2e14769793256e7b42a
ars <pavlovskiy.nikita@gmail.com>
2017-06-30 00:16:58 +0300

    SSL

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const core = require('./core');
 
-core.app
-    .use(core.router.routes())
-    .listen(require('./config').port);
+core.app.use(core.router.routes());
+
+http.createServer(core.app.callback()).listen(80);
+const options = {
+    key: fs.readFileSync('./ssl/private.pem', 'utf8'),
+    cert: fs.readFileSync('./ssl/20180303.crt', 'utf8')
+};
+https.createServer(options, core.app.callback()).listen(443);
