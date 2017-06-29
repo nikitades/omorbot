6414a60fd20b0b2750873f41884bebb2e2813a36
ars <pavlovskiy.nikita@gmail.com>
2017-06-30 00:26:04 +0300

    SSL 2

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ core.app.use(core.router.routes());
 http.createServer(core.app.callback()).listen(80);
 const options = {
     key: fs.readFileSync('./ssl/private.pem', 'utf8'),
-    cert: fs.readFileSync('./ssl/20180303.crt', 'utf8')
+    cert: fs.readFileSync('./ssl/cert.crt', 'utf8')
 };
 https.createServer(options, core.app.callback()).listen(443);
